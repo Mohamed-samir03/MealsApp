7a2dc0088e5a36a226e0d5efb633ab3bd7c8a089
mo samir <smyr03130@gmail.com>
2023-03-16 21:23:59 +0200

    version 1

---
+++ app/src/main/java/com/example/mealzapp/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.mealzapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.RecyclerView
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    private val viewModel:MealsViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mealsAdapter = MealsAdapter()
+        val rv = findViewById<RecyclerView>(R.id.category_rv)
+        viewModel.getMeals()
+
+        lifecycleScope.launch {
+            viewModel.categories.collect{
+                mealsAdapter.submitList(it?.categories)
+                rv.adapter = mealsAdapter
+            }
+        }
+
+    }
+}
